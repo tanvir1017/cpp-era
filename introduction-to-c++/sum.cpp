dbf454412503daa092d8cfba764837c9ad6a1ab2
tanvir1017 <developer.tanvirhossain@gmail.com>
2025-07-18 16:56:57 +0600

    feat: 🎸 Writing simple function for summition of two numbers

---
+++ introduction-to-c++/sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int  main(){
+    double a, b;
+    cout << "Please enter first number" << endl;
+    cin >> a;
+    cout << "Please enter last number" << endl;
+    cin >> b;
+
+    cout << "Sum first and last number is = " << (a + b) << endl;
+    return 0;
+}
