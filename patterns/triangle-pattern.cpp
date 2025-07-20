2b6ab786849e997b68a56d6066b7636ca851ba1b
tanvir1017 <developer.tanvirhossain@gmail.com>
2025-07-21 02:17:20 +0600

    feat: 🎸 inverted inner triangle loop

--- patterns/triangle-pattern.cpp
+++ patterns/triangle-pattern.cpp
@@ -35,7 +35,7 @@ using namespace std;
  * 1 2 3 4
  * */
 
- int main(){
+/*  int main(){
      int n = 4;
      for (int i = 0; i <n; i++){
          for (int j = i + 1; j > 0 ; j--){
@@ -45,4 +45,17 @@ using namespace std;
      }
 
      return 0;
- }
+ } */
+
+
+int main(){
+    int n = 4;
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j > 0 ; j--){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
